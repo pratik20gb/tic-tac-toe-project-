21d6abe1b2cd65f77a50348dcc1fecdc61d6c53c
PRATIK RAJ <95163033+Pratikcodex@users.noreply.github.com>
2022-08-13 07:12:25 +0000

    my game

--- main.py
+++ main.py
@@ -1,86 +1,51 @@
-import os
+def sum(a, b, c ):
+    return a + b + c
 
-def printBoard(gameValues):
-    # Printing Board By using gameValues 's List
-    print(f" {gameValues[0]} | {gameValues[1]} | {gameValues[2]} ")
-    print(f"---|---|---")
-    print(f" {gameValues[3]} | {gameValues[4]} | {gameValues[5]} ")
-    print(f"---|---|---")
-    print(f" {gameValues[6]} | {gameValues[7]} | {gameValues[8]} ")
+def printBoard(xState, zState):
+    zero = 'X' if xState[0] else ('O' if zState[0] else 0)
+    one = 'X' if xState[1] else ('O' if zState[1] else 1)
+    two = 'X' if xState[2] else ('O' if zState[2] else 2)
+    three = 'X' if xState[3] else ('O' if zState[3] else 3)
+    four = 'X' if xState[4] else ('O' if zState[4] else 4)
+    five = 'X' if xState[5] else ('O' if zState[5] else 5)
+    six = 'X' if xState[6] else ('O' if zState[6] else 6)
+    seven = 'X' if xState[7] else ('O' if zState[7] else 7)
+    eight = 'X' if xState[8] else ('O' if zState[8] else 8)
+    print(f"{zero} | {one} | {two} ")
+    print(f"--|---|---")
+    print(f"{three} | {four} | {five} ")
+    print(f"--|---|---")
+    print(f"{six} | {seven} | {eight} ") 
 
-def checkWin(gameValues):
-    # All Winning Patterns
+def checkWin(xState, zState):
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-
     for win in wins:
-        # if gameValues matches with the pattern and has X then X won the Match
-        if(gameValues[win[0]] == gameValues[win[1]] == gameValues[win[2]] == 'X'):
-            printBoard(gameValues)
+        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
             print("X Won the match")
             return 1
-
-        # if gameValues matches with the pattern and has X then O won the Match
-        if(gameValues[win[0]] == gameValues[win[1]] == gameValues[win[2]] == 'O'):
-            printBoard(gameValues)
+        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
             print("O Won the match")
             return 0
-
-        # if all places are filled and no one is the winner
-        if all(isinstance(item, str) for item in gameValues):
-            printBoard(gameValues)
-            return -2
-    # return -1 if no one wons
     return -1
-
-if __name__ == '__main__':
-    print("Welcome to the battlefield")
-    gameValues=[0, 1, 2, 3, 4, 5, 6, 7, 8]
-    chance = 1
-
+    
+if __name__ == "__main__":
+    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1 for X and 0 for O
+    print("Welcome abord gamer")
     while(True):
-        try:
-            if chance == 1:
-                printBoard(gameValues)
-                print("\nX's Chance")
-                value = int(input("\nPlease enter a value: "))
-
-                # check if already filled with O
-                if gameValues[value]!= 'O':
-                    gameValues[value] = 'X'
-                else:
-                    os.system('CLS')
-                    print("\nPlease Enter Different Location for X")
-                    continue
-                os.system('CLS')
-
-            if chance == 0:
-                printBoard(gameValues)
-                print("\nZ's Chance")
-                value = int(input("\nPlease enter a value: "))
-
-                # check if already filled with X
-                if gameValues[value]!= 'X':
-                    gameValues[value] = 'O'
-                else:
-                    os.system('CLS')
-                    print("\nPlease Enter Different Location for O")
-                    continue
-                os.system('CLS')
-
-        except IndexError:
-            # exception if Value is not between 0 to 8 
-            os.system('CLS')
-            print("\nOops!! Please Enter value from 0 - 8\n")
-            continue
-
-        # for giving chance to other player
-        chance = 1 - chance
-        cwin = checkWin(gameValues)
-        # Game Draw
-        if(cwin == -2):
-            print("Game Draw")
-            break
-        # Match Over
+        printBoard(xState, zState)
+        if(turn == 1):
+            print("X's Chance")
+            value = int(input("Please enter a value: "))
+            xState[value] = 1
+        else:
+            print("O's Chance")
+            value = int(input("Please enter a value: "))
+            zState[value] = 1
+        cwin = checkWin(xState, zState)
         if(cwin != -1):
             print("Match over")
             break
+        
+        turn = 1 - turn
